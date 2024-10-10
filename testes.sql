48615d7c1af7c27c84cd38db76f06960ece41ab8
Francisco Nardi <francisnardi@gmail.com>
2024-10-10 12:31:27 -0300

    Create testes.sql

---
+++ testes.sql
@@ -0,0 +1,17 @@
+INSERT INTO TB_RISKBASED_FXNATUREEXCEPTION (
+    IdRiskBasedFxNatureException, 
+    IdRiskBasedDiligence, 
+    FxNatureCodeCategory, 
+    FxNatureName, 
+    BuySell, 
+    Disclaimer, 
+    Comments, 
+    isActive, 
+    ReferenceDate, 
+    UserLastUpdate, 
+    DateLastUpdate
+)
+VALUES
+    (valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10, valor11),
+    -- Continue listando os registros até o 82º
+    (valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, valor9, valor10, valor11);
